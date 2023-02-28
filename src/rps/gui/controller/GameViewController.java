f3d4a6670e43e244320577835f08957dedc8bd01
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2023-02-28 12:57:48 +0100

    alertbox that gets player name

--- src/rps/gui/controller/GameViewController.java
+++ src/rps/gui/controller/GameViewController.java
@@ -4,9 +4,13 @@ package rps.gui.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 /**
@@ -15,6 +19,8 @@ import java.util.ResourceBundle;
  */
 public class GameViewController implements Initializable {
 
+    @FXML
+    private Text txtPlayer;
     @FXML
     private ImageView playerImage;
     @FXML
@@ -25,7 +31,7 @@ public class GameViewController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        getUserName();
     }
 
     @FXML
@@ -38,5 +44,14 @@ public class GameViewController implements Initializable {
 
     @FXML
     private void handleScissor(ActionEvent actionEvent) {
+    }
+
+    private void getUserName(){
+        TextInputDialog getPlayerName = new TextInputDialog();
+        getPlayerName.setTitle("Choose Name");
+        getPlayerName.setHeaderText("Please enter your name:");
+        getPlayerName.setContentText("Name:");
+        Optional<String> result = getPlayerName.showAndWait();
+        txtPlayer.setText(result.get());
     }
 }
